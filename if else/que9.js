9052dfdac34fe414b32aaa21401dc8e0891b4959
8501kaminipatel <8501kaminipatel@gmail.com>
2024-10-24 17:51:55 +0530

    Add files via upload

---
+++ if else/que9.js
@@ -0,0 +1,8 @@
+// Vowels: a, e, i, o, u
+var char = "z";
+
+if (char == "a" || char == "e" || char == "i" || char == "o" || char == "u") {
+  console.log("Vowel");
+} else {
+  console.log("Not a vowel");
+}
